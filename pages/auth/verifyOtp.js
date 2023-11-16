43f600c0087e787ea62f2b60287b6ae37f13ea12
Jobr <delivery@jobr.tech>
2023-11-16 16:11:38 +0530

    changes

--- pages/auth/verifyOtp.js
+++ pages/auth/verifyOtp.js
@@ -1,10 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import * as Images from "../../utilities/images";
 import Image from "next/image";
 import Link from 'next/link';
-
+import OTPInput from 'react-otp-input';
 
 const VerifyOtp = () => {
+    const [otp, setOtp] = useState("");
+    const generateRandomName = () => {
+        return Math.random().toString(36).substr(2, 10);
+    };
+    const onComplete = (code) => {
+
+        // Validate the input to allow only numeric characters
+        const regex = /^[0-9]*$/; // Regular expression to allow only numbers
+
+        if (regex.test(code) || code === '') {
+            setOtp(code);
+        }
+    }
     return (
         <>
             <div className='verifyOtpSection verificationSection'>
@@ -12,13 +25,34 @@ const VerifyOtp = () => {
                     <h1 className='verifyHeading'> Let’s verify your phone <br /> number.</h1>
                     <h4 className='verifySub'>Enter the code we’ve sent to +1 (438) 000-0000</h4>
                     <form>
-                       <div className='otpMain'></div>
+                        <div className='otpMain'>
+                            <div className="verify-part">
+                                <div className="verify-box text-center">
+                                    <div className="pin-box d-flex justify-content-center" >
+                                        <OTPInput numInputs={5}
+                                            className='input_digits_'
+                                            value={otp}
+                                            data-cy="pin-field"
+                                            name={generateRandomName()}
+                                            autoComplete="new-password"
+                                            isInputNum={true}
+                                            isInputSecure={true}
+                                            renderInput={(props) => <input {...props} type="password" maxLength={5} />}
+                                            onChange={onComplete}
+                                        // onComplete={(code) => onComplete(code)}
+                                        />
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                         <div className='verifyBtn'>
                             <button className='backverifyBtn w-100' type='submit'>
                                 <Image src={Images.DarkLeft} alt="image" className="img-fluid leftImg" />
                                 Back
                             </button>
-                            <Link href="#" className='verifyTime w-100'>29s to resend code</Link>
+
+                            {/* <Link href="#" className='verifyTime w-100'>29s to resend code</Link> */}
+                            <Link href="#" className='verifyTime w-100'>Resend</Link>
                         </div>
                     </form>
                 </div>
