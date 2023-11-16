995daa031ac34cb82b9575ee15b7c1ce5ad8c25d
Reetu <reetu.saini@itechnolabs.tech>
2023-11-16 13:54:54 +0530

    otp design

---
+++ pages/auth/verifyOtp.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import * as Images from "../../utilities/images";
+import Image from "next/image";
+import Link from 'next/link';
+
+
+const VerifyOtp = () => {
+    return (
+        <>
+            <div className='verifyOtpSection verificationSection'>
+                <div className='verifyBox'>
+                    <h1 className='verifyHeading'> Let’s verify your phone <br /> number.</h1>
+                    <h4 className='verifySub'>Enter the code we’ve sent to +1 (438) 000-0000</h4>
+                    <form>
+                       <div className='otpMain'></div>
+                        <div className='verifyBtn'>
+                            <button className='backverifyBtn w-100' type='submit'>
+                                <Image src={Images.DarkLeft} alt="image" className="img-fluid leftImg" />
+                                Back
+                            </button>
+                            <Link href="#" className='verifyTime w-100'>29s to resend code</Link>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default VerifyOtp
--- utilities/images.js
+++ utilities/images.js
@@ -1,26 +1,8 @@
-import BrandLogo from "../public/images/BrandLogo.png"
-import Addsquarelight from "../public/images/Add_square_light.svg";
-import AddsquarelightDark from "../public/images/Add_square_lightDark.svg";
-// import deliveryOrder from "../app/public/images/deliveryOrder.svg";
-// import deliveryOrderDark from "../app/public/images/deliveryOrderDark.svg";
-// import shippingOrder from "../app/public/images/shippingOrder.svg";
-// import shippingOrderDark from "../app/public/images/shippingOrderDark.svg";
-// import calendar from "../app/public/images/calendar.svg";
-// import analytics from "../app/public/images/analytics.svg";
-// import analyticsIn from "../app/public/images/analyticsIn.svg";
-// import analyticsDark from "../app/public/images/analyticsDark.svg";
-// import wallet from "../app/public/images/wallet.svg";
-// import walletDark from "../app/public/images/walletDark.svg";
-// import tray from "../app/public/images/tray.svg";
-// import trayDark from "../app/public/images/trayDark.svg";
-// import customerD from "../app/public/images/customerD.svg";
 import ArrowLeft from "../public/images/arrowLeft.svg";
 import ArrowRight from "../public/images/arrowRight.svg";
-
+import DarkLeft from "../public/images/DarkLeft.svg"
 export{
-    BrandLogo,
-    Addsquarelight,
-    AddsquarelightDark,
     ArrowLeft,
     ArrowRight,
+    DarkLeft,
 };
