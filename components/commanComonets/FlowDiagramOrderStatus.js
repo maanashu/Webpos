bb835ff99e6262823ed9dc669c59dc0e061f20ff
Harwinder Singh <harwinder.singh@itechnolabs.tech>
2023-12-12 19:40:38 +0530

    add order status flow

--- components/commanComonets/FlowDiagramOrderStatus.js
+++ components/commanComonets/FlowDiagramOrderStatus.js
@@ -1,6 +1,13 @@
 import Image from "next/image";
 import React from "react";
-import { orderBike } from "../../utilities/images";
+import {
+  dotAssignDriver,
+  dotOrderAccepted,
+  dotOrderDelivered,
+  dotOrderPickup,
+  orderBike,
+  packageBoxPurplish,
+} from "../../utilities/images";
 
 const FlowDiagramOrderStatus = () => {
   return (
@@ -23,32 +30,118 @@ const FlowDiagramOrderStatus = () => {
           Order Status
         </p>
       </div>
-      <div>
-        {[
-          { status: "Verify Code", time: "7893 5623" },
-          { status: "Delivered", time: "Within 10 mintues" },
-          {
-            status: "Product Pickup",
-            time: "Within 10 mintues",
-            id: "JOBR-3432",
-          },
-          { status: "Assign Driver", time: "21 Jun, 2022 | 10:02 am" },
-          { status: "Ready to Pickup", time: "21 Jun, 2022 | 12:09 am" },
-          {
-            status: "Order Accepted",
-            time: "21 Jun, 2022 | 02:10 am",
-            backgroundColor: "rgba(240, 12, 6, 1)",
-          },
-        ].map((el) => (
-          <div>
-            <div className="status-dots-container-customers">
+      <div
+        style={{
+          display: "flex",
+          gap: "13px",
+        }}
+      >
+        <div
+          style={{
+            gap: "10px",
+            display: "flex",
+            alignItems: "center",
+            flexDirection: "column",
+          }}
+        >
+          {[
+            dotAssignDriver,
+            dotOrderDelivered,
+            dotOrderPickup,
+            dotAssignDriver,
+            dotOrderDelivered,
+            dotOrderAccepted,
+          ].map((el, idx) => (
+            <>
+              <Image
+                src={el}
+                style={{ borderRadius: "50%" }}
+              />
+              {idx !== 5 && (
+                <div
+                  style={{
+                    width: "1px",
+                    height: "20px",
+                    background: "#636E9F",
+                  }}
+                />
+              )}
+            </>
+          ))}
+        </div>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            gap: "22px",
+          }}
+        >
+          {[
+            { status: "Verify Code", time: "7893 5623" },
+            { status: "Delivered", time: "Within 10 mintues" },
+            {
+              status: "Product Pickup",
+              time: "Within 10 mintues",
+              id: "JOBR-3432",
+            },
+            { status: "Assign Driver", time: "21 Jun, 2022 | 10:02 am" },
+            { status: "Ready to Pickup", time: "21 Jun, 2022 | 12:09 am" },
+            {
+              status: "Order Accepted",
+              time: "21 Jun, 2022 | 02:10 am",
+              backgroundColor: "rgba(240, 12, 6, 1)",
+            },
+          ].map((el, idx) => (
+            <div
+              style={{
+                display: "flex",
+                gap: "13px",
+                alignItems: "center",
+              }}
+            >
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  gap: "3px",
+                }}
+              >
+                <p
+                  className="main-text-color-styles-customers"
+                  style={{
+                    fontWeight: "400",
+                    fontSize: "14px",
+                  }}
+                >
+                  {el.status}
+                </p>
+                <p
+                  style={{
+                    color: idx == 0 ? "#58C2E8" : "#7e8ac1",
+                    fontSize: "12px",
+                    fontWeight: idx == 0 ? "600" : "400",
+                  }}
+                >
+                  {el.time}
+                </p>
+              </div>
+              {el?.id && (
+                <div className="flex-row-space-between order-status-pickup-customer">
+                  <Image src={packageBoxPurplish} />
+                  <p
+                    className="main-text-color-styles-customers"
+                    style={{
+                      color: "#7233C2",
+                      fontSize: "12px",
+                    }}
+                  >
+                    {el?.id}
+                  </p>
+                </div>
+              )}
             </div>
-            <div>
-              <p>{el.status}</p>
-              <p>{el.time}</p>
-            </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
     </div>
   );
