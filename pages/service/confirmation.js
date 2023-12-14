43e98483a5127ed53878be5bf524652380993c6f
Reetu <reetu.saini@itechnolabs.tech>
2023-12-14 10:34:47 +0530

    changes

--- pages/service/confirmation.js
+++ pages/service/confirmation.js
@@ -11,8 +11,8 @@ const Confirmation = () => {
                         <div className='commanOuter me-0 commonSubOuter fullCartLeft'>
                             <div className='fullCartInfo'>
                                 <div className='appointmentHeading'>
-                                    <Image src={Images.boldLeftArrow} alt="leftarrow image" className="img-fluid" />
-                                    <h4 className='appointMain ms-2'>Full Cart</h4>
+                                    <Image src={Images.boldLeftArrow} alt="leftarrow image" className="img-fluid arrowShort" />
+                                    <h4 className='confirmBack ms-2'>Back</h4>
                                 </div>
                             </div>
                         </div>
