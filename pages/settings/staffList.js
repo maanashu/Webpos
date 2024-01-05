6b0c04e69255146e250168543c39e09657386fbf
Reetu <reetu.saini@itechnolabs.tech>
2024-01-05 12:07:08 +0530

    changes

---
+++ pages/settings/staffList.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { ListGroup, ListGroupItem } from 'react-bootstrap';
+import * as Images from "../../utilities/images"
+import Image from "next/image";
+import Link from 'next/link';
+
+const StaffList = () => {
+    return (
+        <>
+            <div className='settingMain staffSection'>
+
+            </div>
+        </>
+    )
+}
+
+export default StaffList
