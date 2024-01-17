2685197cc6fd1292ce787914268ea2a9ccb464e9
Reetu <reetu.saini@itechnolabs.tech>
2024-01-17 11:35:38 +0530

    notification page Design

--- pages/settings/notification/index.js
+++ pages/settings/notification/index.js
@@ -3,13 +3,167 @@ import React from 'react'
 const Notification = () => {
   return (
     <>
-        <div className='notificationSection settingOuter'>
-            <div className='notifySub'>
-                <h4 className='cancelOrderText'>App Notifications</h4>
-                <hr className='cartDivide'/>
-                <div className='notifyCheck'></div>
+      <div className='notificationSection settingOuter'>
+        <div className='notifySub'>
+          <h4 className='cancelOrderText'>App Notifications</h4>
+          <hr className='cartDivide' />
+          <div className='notifyCheck'>
+            <h4 className='appointSub m-0'>New Order</h4>
+            <div className="roundCheck mb-0">
+              <input type="checkbox" />
+              <label className='amountText d-none '></label>
             </div>
+          </div>
+          <div className='notifyCheck'>
+            <h4 className='appointSub m-0'>Cancelled Order</h4>
+            <div className="roundCheck mb-0">
+              <input type="checkbox" />
+              <label className='amountText d-none '></label>
+            </div>
+          </div>
+          <div className='notifyCheck'>
+            <h4 className='appointSub m-0'>Low Stock Warning</h4>
+            <div className="roundCheck mb-0">
+              <input type="checkbox" />
+              <label className='amountText d-none '></label>
+            </div>
+          </div>
+          <div className='notifyCheck'>
+            <h4 className='appointSub m-0'>Out-of-Stock Alert</h4>
+            <div className="roundCheck mb-0">
+              <input type="checkbox" />
+              <label className='amountText d-none '></label>
+            </div>
+          </div>
+          <div className='notifyCheck'>
+            <h4 className='appointSub m-0'>Daily Sales Summary</h4>
+            <div className="roundCheck mb-0">
+              <input type="checkbox" />
+              <label className='amountText d-none '></label>
+            </div>
+          </div>
+          <div className='notifyCheck'>
+            <h4 className='appointSub m-0'>Unusual Sales Activity:</h4>
+            <div className="roundCheck mb-0">
+              <input type="checkbox" />
+              <label className='amountText d-none '></label>
+            </div>
+          </div>
+          <div className='notifyCheck'>
+            <h4 className='appointSub m-0'>Employee Logins</h4>
+            <div className="roundCheck mb-0">
+              <input type="checkbox" />
+              <label className='amountText d-none '></label>
+            </div>
+          </div>
         </div>
+        <div className='notifySub'>
+          <h4 className='cancelOrderText'>SMS Notification</h4>
+          <hr className='cartDivide' />
+          <div className='notifyCheck'>
+            <h4 className='appointSub m-0'>New Order</h4>
+            <div className="roundCheck mb-0">
+              <input type="checkbox" />
+              <label className='amountText d-none '></label>
+            </div>
+          </div>
+          <div className='notifyCheck'>
+            <h4 className='appointSub m-0'>Cancelled Order</h4>
+            <div className="roundCheck mb-0">
+              <input type="checkbox" />
+              <label className='amountText d-none '></label>
+            </div>
+          </div>
+          <div className='notifyCheck'>
+            <h4 className='appointSub m-0'>Low Stock Warning</h4>
+            <div className="roundCheck mb-0">
+              <input type="checkbox" />
+              <label className='amountText d-none '></label>
+            </div>
+          </div>
+          <div className='notifyCheck'>
+            <h4 className='appointSub m-0'>Out-of-Stock Alert</h4>
+            <div className="roundCheck mb-0">
+              <input type="checkbox" />
+              <label className='amountText d-none '></label>
+            </div>
+          </div>
+          <div className='notifyCheck'>
+            <h4 className='appointSub m-0'>Daily Sales Summary</h4>
+            <div className="roundCheck mb-0">
+              <input type="checkbox" />
+              <label className='amountText d-none '></label>
+            </div>
+          </div>
+          <div className='notifyCheck'>
+            <h4 className='appointSub m-0'>Unusual Sales Activity:</h4>
+            <div className="roundCheck mb-0">
+              <input type="checkbox" />
+              <label className='amountText d-none '></label>
+            </div>
+          </div>
+          <div className='notifyCheck'>
+            <h4 className='appointSub m-0'>Employee Logins</h4>
+            <div className="roundCheck mb-0">
+              <input type="checkbox" />
+              <label className='amountText d-none '></label>
+            </div>
+          </div>
+        </div>
+        <div className='notifySub'>
+          <h4 className='cancelOrderText'>E-mail Notification</h4>
+          <hr className='cartDivide' />
+          <div className='notifyCheck'>
+            <h4 className='appointSub m-0'>New Order</h4>
+            <div className="roundCheck mb-0">
+              <input type="checkbox" />
+              <label className='amountText d-none '></label>
+            </div>
+          </div>
+          <div className='notifyCheck'>
+            <h4 className='appointSub m-0'>Cancelled Order</h4>
+            <div className="roundCheck mb-0">
+              <input type="checkbox" />
+              <label className='amountText d-none '></label>
+            </div>
+          </div>
+          <div className='notifyCheck'>
+            <h4 className='appointSub m-0'>Low Stock Warning</h4>
+            <div className="roundCheck mb-0">
+              <input type="checkbox" />
+              <label className='amountText d-none '></label>
+            </div>
+          </div>
+          <div className='notifyCheck'>
+            <h4 className='appointSub m-0'>Out-of-Stock Alert</h4>
+            <div className="roundCheck mb-0">
+              <input type="checkbox" />
+              <label className='amountText d-none '></label>
+            </div>
+          </div>
+          <div className='notifyCheck'>
+            <h4 className='appointSub m-0'>Daily Sales Summary</h4>
+            <div className="roundCheck mb-0">
+              <input type="checkbox" />
+              <label className='amountText d-none '></label>
+            </div>
+          </div>
+          <div className='notifyCheck'>
+            <h4 className='appointSub m-0'>Unusual Sales Activity:</h4>
+            <div className="roundCheck mb-0">
+              <input type="checkbox" />
+              <label className='amountText d-none '></label>
+            </div>
+          </div>
+          <div className='notifyCheck'>
+            <h4 className='appointSub m-0'>Employee Logins</h4>
+            <div className="roundCheck mb-0">
+              <input type="checkbox" />
+              <label className='amountText d-none '></label>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
