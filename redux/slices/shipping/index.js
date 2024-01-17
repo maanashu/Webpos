a1d3b0bf7c636161c13abf1cb827bd529764e640
Jobr <delivery@jobr.tech>
2024-01-17 15:51:48 +0530

    changes

--- redux/slices/shipping/index.js
+++ redux/slices/shipping/index.js
@@ -1,30 +1,29 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  sidebarCountData: {},
-  loading: false,
+    sidebarCountData: {},
+    loading: false,
 };
 
 export const shippingSlice = createSlice({
-  name: "customers",
-  initialState,
-  reducers: {
-    getShippingsSidebarCount: (state) => {
-      state.loading = true;
+    name: "shipping",
+    initialState,
+    reducers: {
+        getShippingsSidebarCount: (state) => {
+            state.loading = true;
+        },
+        setSidebarCountData: (state, action) => {
+            state.loading = false;
+            state.sidebarCountData = action?.payload;
+        }
     },
-    setSidebarCountData: (state, action) => {
-      state.loading = false;
-      state.sidebarCountData = action?.payload;
-    }
-},
 });
 
 // Action creators are generated for each case reducer function
 export const {
     getShippingsSidebarCount,
     setSidebarCountData
-  } = shippingSlice.actions;
-  
-  export const selectsShippingData = (state) => state.customers;
-  
-  export default shippingSlice.reducer;
+} = shippingSlice.actions;
+
+export const selectsShippingData = (state) => state.shipping;
+export default shippingSlice.reducer;
